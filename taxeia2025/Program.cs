9faf590a53f3a71ec0ca5954e5a968e48a70960c
Angelos Berketis <91057664+Aparaititos@users.noreply.github.com>
2025-10-07 16:52:18 +0300

    Minor changes, photo fixes etc.

--- taxeia2025/Program.cs
+++ taxeia2025/Program.cs
@@ -14,10 +14,9 @@ namespace Peripatos_UI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             Database.Connect_PeripatosDB();
-            //1. Delete the PlaceImage table.
-            //2. Insert images with place name as folder name in Beach/Images.
-            //3. run the following method.
-           // Database.ONETIME_Insert_PlaceImage("Restaurant");
+            //Database.ONETIME_Insert_PlaceImage("Beaches");
+            //Database.ONETIME_Insert_PlaceImage("Restaurant");
+            //Database.ONETIME_Insert_PlaceImage("Sight");
             AppList_Manager.Initialize_Lists();
 
             ApplicationConfiguration.Initialize();
